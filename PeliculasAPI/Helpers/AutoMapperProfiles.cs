f37a297b387db876b1a7662b247719dda3c29832
Jorge LLanque <georgellanque@gmail.com>
2022-02-03 01:49:47 -0400

    udpate changes

--- PeliculasAPI/Helpers/AutoMapperProfiles.cs
+++ PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -19,6 +19,11 @@ namespace PeliculasAPI.Helpers
             CreateMap<ActorCreacionDTO, Actor>()
                 .ForMember(x => x.Foto, options => options.Ignore());
             CreateMap<ActorPatchDTO, Actor>().ReverseMap();
+
+            CreateMap<Pelicula, PeliculaDTO>().ReverseMap();
+            CreateMap<PeliculaCreacionDTO, Pelicula>()
+                .ForMember(x => x.Poster, options => options.Ignore());
+            CreateMap<PeliculaPatchDTO, Pelicula>().ReverseMap();
         }
     }
 }
